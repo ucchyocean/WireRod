da18545fb58d8fd9f220f1c6adebf29886ad8883
ucchy <ocean163@hotmail.com>
2020-05-07 21:56:38 +0900

    v3.2.1 : Fixed issue #4

--- src/main/java/com/github/ucchyocean/WireRod.java
+++ src/main/java/com/github/ucchyocean/WireRod.java
@@ -5,24 +5,19 @@
  */
 package com.github.ucchyocean;
 
-import com.github.ucchyocean.ct.ColorTeaming;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * ワイヤロッドプラグイン
- * 
+ *
  * @author ucchy
  */
 public class WireRod extends JavaPlugin {
-
-    private static final String NAME = "wirerod";
 
     private static WireRod instance;
 
@@ -30,7 +25,7 @@ public class WireRod extends JavaPlugin {
 
     /**
      * プラグインが有効になったときに呼び出されるメソッド
-     * 
+     *
      * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
      */
     public void onEnable() {
@@ -43,31 +38,6 @@ public class WireRod extends JavaPlugin {
         if (config.isEnableCraft()) {
             WireRodUtil.addRecipe();
         }
-
-        loadColorTeaming();
-    }
-
-    private void loadColorTeaming() {
-        if (!getServer().getPluginManager().isPluginEnabled("ColorTeaming")) {
-            return;
-        }
-
-        Plugin colorteaming = getServer().getPluginManager().getPlugin("ColorTeaming");
-        if (!(colorteaming instanceof ColorTeaming)) {
-            return;
-        }
-
-        String ctversion = colorteaming.getDescription().getVersion();
-        if (!isUpperVersion(ctversion, "2.2.5")) {
-            getLogger().warning("ColorTeaming was too old. The cooperation feature will be disabled.");
-            getLogger().warning("NOTE: Please use ColorTeaming v2.2.5 or later version.");
-            return;
-        }
-
-        getLogger().info(
-                "ColorTeaming was loaded. " + getDescription().getName() + " is in cooperation with ColorTeaming.");
-        new ColorTeamingBridge((ColorTeaming) colorteaming)
-                .registerItem(WireRodUtil.getWireRod(0), NAME, WireRodUtil.DISPLAY_NAME);
     }
 
     static WireRod getInstance() {
@@ -84,7 +54,7 @@ public class WireRod extends JavaPlugin {
 
     /**
      * コマンドが実行されたときに呼び出されるメソッド
-     * 
+     *
      * @see org.bukkit.plugin.java.JavaPlugin#onCommand(org.bukkit.command.CommandSender,
      *      org.bukkit.command.Command, java.lang.String, java.lang.String[])
      */
@@ -115,7 +85,7 @@ public class WireRod extends JavaPlugin {
 
             return true;
         }
-        
+
         if (args[0].equalsIgnoreCase("get")) {
 
             if (!(sender instanceof Player)) {
@@ -139,7 +109,7 @@ public class WireRod extends JavaPlugin {
 
             return true;
         }
-        
+
         if (args.length >= 2 && args[0].equalsIgnoreCase("give")) {
 
             if (!sender.hasPermission("wirerod.give")) {
@@ -164,53 +134,5 @@ public class WireRod extends JavaPlugin {
         }
 
         return false;
-    }
-
-
-    /**
-     * 指定されたバージョンが、基準より新しいバージョンかどうかを確認する<br>
-     * 完全一致した場合もtrueになることに注意。
-     * 
-     * @param version 確認するバージョン
-     * @param border  基準のバージョン
-     * @return 基準より確認対象の方が新しいバージョンかどうか
-     */
-    private boolean isUpperVersion(String version, String border) {
-
-        int hyphen = version.indexOf("-");
-        if (hyphen > 0) {
-            version = version.substring(0, hyphen);
-        }
-
-        String[] versionArray = version.split("\\.");
-        int[] versionNumbers = new int[versionArray.length];
-        for (int i = 0; i < versionArray.length; i++) {
-            if (!versionArray[i].matches("[0-9]+"))
-                return false;
-            versionNumbers[i] = Integer.parseInt(versionArray[i]);
-        }
-
-        String[] borderArray = border.split("\\.");
-        int[] borderNumbers = new int[borderArray.length];
-        for (int i = 0; i < borderArray.length; i++) {
-            if (!borderArray[i].matches("[0-9]+"))
-                return false;
-            borderNumbers[i] = Integer.parseInt(borderArray[i]);
-        }
-
-        int index = 0;
-        while ((versionNumbers.length > index) && (borderNumbers.length > index)) {
-            if (versionNumbers[index] > borderNumbers[index]) {
-                return true;
-            } else if (versionNumbers[index] < borderNumbers[index]) {
-                return false;
-            }
-            index++;
-        }
-        if (borderNumbers.length == index) {
-            return true;
-        } else {
-            return false;
-        }
     }
 }
